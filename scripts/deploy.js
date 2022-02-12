2980f3d844dd4fba1d33cbbe80ef8b146f49bd9b
Zach Cook <zach@web3technologies.io>
2022-02-12 14:11:22 -0600

    updated the deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -24,39 +24,9 @@ async function main() {
 }
 
 function moveArtifacts(nft){
-    // console.log("nft deployed to: ", nft.address);
-    // console.log("moving artifacts...")
     let parent = path.resolve(__dirname, '..')
     const frontEndPath = path.resolve(parent, "../drinking_trees_frontend")
-    console.log(parent)
-    // // console.log(path.resolve(parent, "/drinking_trees_frontend"))
-
-    // console.log("this: ", path.dirname(process.cwd()).split(path.sep).pop())
-    // console.log("new: ", path.join(process.cwd() + "../drinking_trees_contracts/"))
-    // // let frontEndPath = path.join(path.join(process.cwd() + "../drinking_trees/drinking_trees_frontend"))
-    // console.log("frontend path, :L ", frontEndPath)
-    // // frontEndPath = path.resolve(frontEndPath)
-    // console.log("here")
-    // fs.copyFile(path.resolve(`${process.cwd()}/artifacts`), frontEndPath + "/pages", (err)=> {
-    //     if (err){
-    //         console.log("err")
-    //         throw (err)
-    //     }
-    //     else{
-    //         console.log("copied?")
-    //     }
-    // });
-
-    // console.log("end")
-
-    // To copy a folder or file
-    console.log('copying')
-    console.log(frontEndPath)
-    // const source =  "C:/Users/Crypto Thoughts/Desktop/dev/crypto/drinking_trees/drinking_trees_contracts/artifacts"
-    // const destination =  "C:/Users/Crypto Thoughts/Desktop/dev/crypto/drinking_trees/drinking_trees_frontend"
-    console.log()
-    fse.copySync(process.cwd() + "/artifacts", process.cwd() + "../drinking_trees_frontend");
-    
+    fse.copySync(process.cwd() + "/artifacts", frontEndPath + "/artifacts");
 }
 
 main()
