4e65340beaf05045792eaf8ee13f0ca7524b9fff
Zach Cook <zach@web3technologies.io>
2022-04-04 22:39:22 -0500

    added the mumbai chain

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,35 +3,17 @@ const fs = require("fs")
 require("dotenv").config()
 
 const privateKey = process.env.ACCOUNT1_PRIVATEKEY
-projectId = "50c33544dad04cfe9bdda7f3caa2142a"
-
 
 module.exports = {
   networks: {
     hardhat: {
       chainId: 1337
     },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${projectId}`,
-      chainId: 3,
+    mumbai: {
+      url: `https://rpc-mumbai.maticvigil.com`,
+      chainId: 80001,
       accounts: [privateKey]
     },
-    multivac: {
-      url: `https://rpc.mtv.ac`,
-      chainId: 62621,
-      accounts: [privateKey]
-    }
-    // ,
-    // rinkeby: {
-    //   url: `https://rinkeby.infura.io/v3/${projectId}`,
-    //   chainId: 4,
-    //   accounts: [privateKey]
-    // },
-    // mainnet: {
-    //   url: `https://mainnet.infura.io/v3/${projectId}`,
-    //   chainId: 1,
-    //   accounts: [privateKey]
-    // }
   },
   solidity: "0.8.4",
 };
