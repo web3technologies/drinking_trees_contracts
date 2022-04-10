bda0bcedc11b758d628fa8ee53a7adf2c7c5d3e1
Zach Cook <zach@web3technologies.io>
2022-04-10 15:18:02 -0500

    added the logging capability

--- tasks/lotterymint.js
+++ tasks/lotterymint.js
@@ -1,6 +1,38 @@
 const csv = require("./utils/readCSV.js");
 const getContract = require("./utils/getcontract");
+const log4js = require("log4js");
 
+
+log4js.configure({
+  appenders: {
+    out: { type: 'stdout' }, 
+    success: { 
+        type: "file", 
+        filename: "./logs/lotterymint/success.log" 
+      },
+    error: { 
+          type: "file", 
+          filename: "./logs/lotterymint/error.log" 
+        } 
+    },
+  categories: {
+
+    default: { appenders: [ 'out' ], level: 'trace' },
+
+    success: { 
+        appenders: ["success"], 
+        level: "info" 
+    },
+    error: { 
+        appenders: ["error"], 
+        level: "error" 
+        } 
+    }
+});
+
+const logger = log4js.getLogger()
+const successLogger = log4js.getLogger("success");
+const errorLogger = log4js.getLogger("error");
 
 // This task will be used to mint all of the first 250 NFTs to a users address
 // calls the contracts mintForAddress method itteratively
@@ -10,17 +42,20 @@ async function mint(){
     try {
         const nftContract = await getContract.getContract()
         const lotteryAddresses = await csv.readCSV("./static/lotteryaddresses.csv")
-        
         for(let i = 0; i<lotteryAddresses.length; i++){
             const mintToken = await nftContract.mintForAddress(1, lotteryAddresses[i]);
-            // console.log(`Minted for address: ${lotteryAddresses[i]}`)
+            logger.info(`Minted for address: ${lotteryAddresses[i]} -- Number ${i}`)
+            successLogger.info(`Minted for address: ${lotteryAddresses[i]} -- Number ${i}`)
         }
-
-        console.log("Successful Mint")
-        
+        successLogger.info("Successful Mint")
     } catch(e){
         console.log(e)
         console.log("error")
+        errorLogger.trace("Entering cheese testing");
+        errorLogger.debug("Got cheese.");
+        errorLogger.warn("Cheese is quite smelly.");
+        errorLogger.error("Cheese is too ripe!");
+        errorLogger.fatal("Cheese was breeding ground for listeria.");
     }
 
 }
